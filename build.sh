03eca741210c52837e1638a3a03ec20e2e39f57d
agajdosi <andreas.gajdosik@gmail.com>
2020-10-12 20:30:22 +0200

    Add cross platform building bash script

---
+++ build.sh
@@ -0,0 +1,5 @@
+GOOS=windows go build -o out/windows/tst.exe github.com/agajdosi/twitter-storm-toolkit
+GOOS=darwin go build -o out/darwin/tst github.com/agajdosi/twitter-storm-toolkit
+GOOS=linux go build -o out/linux/tst github.com/agajdosi/twitter-storm-toolkit
+
+
