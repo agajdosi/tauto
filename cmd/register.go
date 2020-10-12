c4c4c0955e9d345563481b091caf3a45f743b517
agajdosi <andreas.gajdosik@gmail.com>
2020-10-12 20:07:01 +0200

    Move register to twitter package

--- cmd/register.go
+++ cmd/register.go
@@ -16,7 +16,7 @@ limitations under the License.
 package cmd
 
 import (
-	"github.com/agajdosi/twitter-storm-toolkit/pkg/register"
+	"github.com/agajdosi/twitter-storm-toolkit/pkg/twitter"
 	"github.com/spf13/cobra"
 )
 
@@ -31,7 +31,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		register.Register()
+		twitter.Register()
 	},
 }
 
--- pkg/twitter/register.go
+++ pkg/twitter/register.go
@@ -1,4 +1,4 @@
-package register
+package twitter
 
 import (
 	"fmt"
