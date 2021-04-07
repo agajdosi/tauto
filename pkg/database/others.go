d08b3ac110b0c312bbd61608d0bd045977f8528c
agajdosi <andreas.gajdosik@gmail.com>
2021-04-07 20:59:20 +0200

    Add database support to retrieve data of others

--- pkg/database/others.go
+++ pkg/database/others.go
@@ -6,6 +6,13 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+//Other holds all the data for the single other account (ally, neutral, enemy)
+type Other struct {
+	ID       int
+	Username string
+	Status   string
+}
+
 //AddOther adds a new other account (ally, neutral or enemy) into the database
 func AddOther(username, status string) (int, error) {
 	location, err := Location()
@@ -42,4 +49,62 @@ func AddOther(username, status string) (int, error) {
 	id, err := result.LastInsertId()
 
 	return int(id), err
+}
+
+//GetOthers gets usernames of all active accounts of others (ally, neutral or enemy) from the database.
+func GetOthers(username, status string) ([]Other, error) {
+	location, err := Location()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sql.Open("sqlite3", location)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	var stmt *sql.Stmt
+	//ideally add support for multiple usernames
+	if username == "" {
+		stmt, err = db.Prepare("select id, username, status from others where status = ? and active = 1")
+	} else {
+		stmt, err = db.Prepare("select id, username, status from others where username = ? and status = ? and active = 1")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	var rows *sql.Rows
+	if username != "" {
+		rows, err = stmt.Query(username)
+	} else {
+		rows, err = stmt.Query()
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var others []Other
+	for rows.Next() {
+		var id int
+		var username string
+		var status string
+		err = rows.Scan(&id, &username, &status)
+		if err != nil {
+			return nil, err
+		}
+		o := Other{id, username, status}
+		others = append(others, o)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return others, err
+	}
+
+	return others, nil
 }
