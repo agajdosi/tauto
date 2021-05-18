1366f33217f17fd2a161afc3547db910aa94a27e
agajdosi <andreas.gajdosik@gmail.com>
2021-05-19 01:57:10 +0200

    Edit build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 GOOS=windows go build -o out/windows/tauto.exe github.com/agajdosi/tauto
-GOOS=darwin go build -o out/darwin/tauto github.com/agajdosi/tauto
-GOOS=linux go build -o out/linux/tauto github.com/agajdosi/tauto
+GOOS=darwin go build -o out/darwin/tauto-mac github.com/agajdosi/tauto
+GOOS=linux go build -o out/linux/tauto-linux github.com/agajdosi/tauto
