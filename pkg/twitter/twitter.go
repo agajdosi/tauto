986dabb265f2accb4e1f47addb44acf760c717bb
agajdosi <andreas.gajdosik@gmail.com>
2021-05-19 01:14:23 +0200

    Add sleeps to login

--- pkg/twitter/twitter.go
+++ pkg/twitter/twitter.go
@@ -48,11 +48,14 @@ func (b Bot) Login() error {
 		//username
 		chromedp.WaitVisible(`//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label/div/div[2]/div/input`, chromedp.BySearch),
 		chromedp.SendKeys(`//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label/div/div[2]/div/input`, b.Username, chromedp.BySearch),
+		chromedp.Sleep(5*time.Second),
 		//password
 		chromedp.WaitVisible(`//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label/div/div[2]/div/input`, chromedp.BySearch),
 		chromedp.SendKeys(`//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label/div/div[2]/div/input`, b.password, chromedp.BySearch),
+		chromedp.Sleep(5*time.Second),
 		//login button
 		chromedp.Click(`//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div/div`, chromedp.BySearch),
+		chromedp.Sleep(5*time.Second),
 	)
 
 	return nil
